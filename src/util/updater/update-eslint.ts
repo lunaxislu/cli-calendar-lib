2ed446a4e2b9c6eaeffcd722fce18639ee3acbfe
lunaxislu <cmk0905@naver.com>
2024-09-16 20:47:51 +0900

    #6 [ next/js ] - next.js js 일때 eslint update

--- src/util/updater/update-eslint.ts
+++ src/util/updater/update-eslint.ts
@@ -108,6 +108,69 @@ export async function updateNextJSEslint(eslintConfig: Config) {
 
       return;
     }
+
+    const project = new Project();
+    const sourceFile = project.addSourceFileAtPath(filepath);
+    const configWithExtends = sourceFile
+      .getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression)
+      .find((objectLiteral: ObjectLiteralExpression) => {
+        return objectLiteral.getProperty("extends") ? objectLiteral : undefined;
+      });
+
+    // extends property 있을 때
+    if (configWithExtends) {
+      const extendsProperty = configWithExtends.getProperty(
+        "extends",
+      ) as PropertyAssignment;
+      const quoteChar = getQuoteChar(configWithExtends);
+      const extendsInitializer = extendsProperty.getInitializer();
+      const kind = extendsInitializer?.getKind();
+      if (kind === SyntaxKind.StringLiteral && extendsInitializer) {
+        // 문자열을 배열로 변환하고, "next/babel" 추가
+        const extendsStringLiteral = extendsInitializer.getText();
+        const newExtendsArray = new Set([
+          extendsStringLiteral,
+          `${quoteChar}next/babel${quoteChar}`,
+        ]); // Set으로 중복 방지
+        extendsProperty.setInitializer(
+          `[${Array.from(newExtendsArray).join(", ")}]`,
+        );
+      }
+      if (kind === SyntaxKind.ArrayLiteralExpression && extendsInitializer) {
+        const arrayElements = extendsInitializer as ArrayLiteralExpression;
+        const elementValues = arrayElements
+          .getElements()
+          .map((el) => el.getText());
+
+        // Set을 사용하여 중복을 방지하고 "next/babel" 추가
+        const newExtendsArray = new Set([
+          ...elementValues,
+          `${quoteChar}next/babel${quoteChar}`,
+        ]); // 기존 값에 "next/babel" 추가
+
+        // 배열로 다시 설정
+        extendsProperty.setInitializer(
+          `[${Array.from(newExtendsArray).join(", ")}]`,
+        );
+      }
+      await sourceFile.save();
+      return;
+    }
+
+    // extends property 없을 때
+    if (!configWithExtends) {
+      const objectLiterals = sourceFile.getDescendantsOfKind(
+        SyntaxKind.ObjectLiteralExpression,
+      )[0];
+
+      const quoteChar = getQuoteChar(objectLiterals);
+      objectLiterals.addPropertyAssignment({
+        name: `extends`, // key 값이므로  quote 안해주기
+        initializer: `${quoteChar}next/babel${quoteChar}`,
+      });
+      await sourceFile.save();
+      return;
+    }
   } catch (err) {
     return console.log(
       `${highlighter.error(
